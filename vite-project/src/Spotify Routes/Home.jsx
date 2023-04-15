06966b7c25c494407755336a87406813d721e4de
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-15 19:28:40 +0100

    Adding mindblowing styles

--- vite-project/src/Spotify Routes/Home.jsx
+++ vite-project/src/Spotify Routes/Home.jsx
@@ -4,6 +4,9 @@ import NavBar from '../Components/NavBar'
 import iconHome from "../images/Vector1.png"
 import iconAlbum from "../images/Vector.png"
 import iconRadio from "../images/Vector3.png"
+import iconLogout from "../images/Vector9.png"
+import iconProfile from "../images/profile.svg"
+import iconVideos from "../images/videos.svg"
 
 const Home = () => {
    return (
@@ -11,9 +14,16 @@ const Home = () => {
          <NavBar></NavBar>
          <div className={HomeStyles["FirstContainer"]}>
             <div className={HomeStyles["NavigateContainer"]}>
-               <img src={iconHome} alt="" />
-               <img src={iconAlbum} alt="" />
-               <img src={iconRadio} alt="" />
+               <div className={HomeStyles["navigate-div-container"]}>
+                  <img src={iconHome} alt="" />
+                  <img src={iconAlbum} alt="" />
+                  <img src={iconRadio} alt="" />
+                  <img src={iconVideos} alt="" />
+               </div>
+               <div className={HomeStyles["personal-info-container"]}>
+                  <img src={iconProfile} alt="" />
+                  <img src={iconLogout} alt="" />
+               </div>
             </div>
          </div>
       </div>
