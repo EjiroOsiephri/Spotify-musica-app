1fc6a52bc23fa29f61f0d2882404ac8197496750
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-15 12:45:52 +0100

    Started signup pages

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,14 @@
+import Signup from "./Routes/Signup"
+import { Route, Routes } from "react-router-dom"
+
 function App() {
-
-
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Signup></Signup>}></Route>
+      </Routes>
+    </>
+  )
 }
 
 export default App
---
+++ vite-project/src/Routes/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../Sass/Signin.module.scss"
+
+const Signup = () => {
+   return (
+      <div className="signup">
+         <div className="typography">
+            <h1>Welcome to Spotify</h1>
+            <h4>Spotify is the leading music app all around the world,you can join us and enjoy this wonderful experience.</h4>
+            <p>Please create an account with us if you haven't done so.</p>
+         </div>
+      </div>
+   )
+}
+
+export default Signup
