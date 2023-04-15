d4e881a8709e5a9e84c36e18b3fdc0ca5f806c15
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-15 13:34:14 +0100

    Login and sign up page looking beautiful

--- vite-project/src/Routes/Signup.jsx
+++ vite-project/src/Routes/Signup.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
-import "../Sass/Signin.module.scss"
+import Styled from "../Sass/Signin.module.scss"
+import Logo from "../images/download12.png"
 
 const Signup = () => {
    return (
-      <div className="signup">
-         <div className="typography">
+      <form className={Styled["signup"]}>
+         <div className={Styled['logo']}>
+            <img src={Logo} alt="" />
+            <h2>Spotify</h2>
+         </div>
+         <div className={Styled["typography"]}>
             <h1>Welcome to Spotify</h1>
             <h4>Spotify is the leading music app all around the world,you can join us and enjoy this wonderful experience.</h4>
             <p>Please create an account with us if you haven't done so.</p>
          </div>
-      </div>
+         <div className={Styled["input-container"]}>
+            <input className={Styled['input']} type="email" placeholder='Email' />
+            <input className={Styled['input']} type="password" placeholder='Password' />
+         </div>
+         <div className={Styled["button-container"]}>
+            <button>Sign up</button>
+         </div>
+      </form >
    )
 }
 
